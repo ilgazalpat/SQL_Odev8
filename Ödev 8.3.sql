56a1d77e6a8936d7c83591f8726e2b913dbba9a6
ilgazalpat <152813471+ilgazalpat@users.noreply.github.com>
2024-01-18 22:20:42 +0100

    SQL Temelleri

---
+++ Ödev 8.3.sql
@@ -0,0 +1,19 @@
+UPDATE employee
+SET name = 'Update_name1' 
+WHERE name = 'Tanner';
+
+UPDATE employee
+SET NAME = 'Update_name2' 
+WHERE name = 'Mahmoud';
+
+UPDATE employee
+SET NAME = 'Update_name3' 
+WHERE name = 'Roch';
+
+UPDATE employee
+SET NAME = 'Update_name4' 
+WHERE name = 'Mitzi';
+
+UPDATE employee
+SET NAME = 'Update_name5' 
+WHERE name = 'Orbadiah';
